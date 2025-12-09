729d83578f7908133ea03d0d21b164f26129ac7f
makonew <makonew.dev@gmail.com>
2025-12-10 00:06:46 +0100

    Small refactor of latest exercise\ latest as in last changed

--- praca-projektowa/src/models/userModel.js
+++ praca-projektowa/src/models/userModel.js
@@ -1,32 +1,13 @@
 const path = require("path");
 const crypto = require("crypto");
 
-const FILE_PATH = path.join(__dirname, "../../data/users.json");
+const {
+    readFileToJson,
+    writeFileToJson
+} = require("../services/fileOperations")
+
 const DEFAULT_OBJECT = { users: [] }
 
-const fs = require("fs");
-
-function readFileToJson(filePath) {
-    try {
-        const data = fs.readFileSync(filePath, "utf8");
-        const json = JSON.parse(data);
-        return json;
-    } catch (err) {
-        console.error("Error:", "Couldn't open file. Possibly doesnt exist yet.");
-        return undefined;
-    }
-}
-
-function writeFileToJson(filePath, obj) {
-    try {
-        const data = JSON.stringify(obj, null, 2);
-        fs.writeFileSync(filePath, data, "utf8");
-        return true;
-    } catch (err) {
-        console.error("Error:", err);
-        return false;
-    }
-}
 
 
 function getUserByUsername(username) {
